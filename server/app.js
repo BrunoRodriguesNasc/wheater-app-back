482359e55ff346b48daa3df33f282328aaae265c
Bruno Rodrigues <bruno_rodrigues977@hotmail.com>
2022-08-02 20:58:06 -0300

    Ajustando cors

--- server/app.js
+++ server/app.js
@@ -3,7 +3,13 @@ const app = express();
 const cors = require("cors");
 const wheater = require('../router/wheater.js');
 const PORT = process.env.PORT || 5000;
-app.options('*', cors()) 
-app.use('/wheater', wheater);
+
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+   
+    next();
+});
+
+app.use('/wheater', wheater);
 app.listen(PORT);
